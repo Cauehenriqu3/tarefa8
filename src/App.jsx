73a4af642ec5b39b4f7792fb512f27aa695d1897
Caue Veloso <131308888+Cauehenriqu3@users.noreply.github.com>
2023-10-03 19:12:58 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {Router} from './components';
+//import Title from './components/Title/Title';
+
+
+function App() {
+
+  return (
+    <Router />
+  );
+}
+
+export default App;
